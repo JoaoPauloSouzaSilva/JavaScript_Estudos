bc7cb89e7561f0ee8eb477ff1bae6b7e1f98236e
JoaoPauloSouzaSilva <joaopaulloss477@gmail.com>
2024-06-28 15:53:39 -0300

    Ex004

--- Exercicos/004/script.js
+++ Exercicos/004/script.js
@@ -1,3 +1,18 @@
-function carregar() {
+function calcular() {
+  var num = document.getElementById('txtn');
+  var numValue = Number(num.value);
 
+  var tab = document.getElementById('seletab');
+  
+  if (num.value.length == 0 ) {
+   alert('Por favor, Digite um Número.');
+  } else {
+    tab.innerHTML = '';
+    for (let i = 1; i <= 10 ; i++) {
+      let item = document.createElement('option');
+      item.value = `tab${i}`;
+      item.text = `${numValue} x ${i} = ${numValue * i}`;
+      tab.appendChild(item);
+    };
+  };
 };
